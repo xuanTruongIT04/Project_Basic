190686d6aab2d6f75fd3e372c17eb4bcf2175e1e
Nguyễn Xuân Trường <nxt160602@gmail.com>
2021-12-21 18:04:10 +0700

    BTL HIT

--- pro_hit/assets/js/app.js
+++ pro_hit/assets/js/app.js
@@ -1,4 +1,40 @@
 $(document).ready(function () {
+  $(".open-res-mn").click(function () {
+    $("#responsive-main-menu").slideToggle(300);
+  });
+
+  $(".icon-open-mn").click(function () {
+    $(".sub-responsive-main-menu").slideToggle(300);
+    $(this).toggleClass("fas fa-chevron-down fas fa-chevron-up");
+  });
+
+  var upSlide = function () {
+    $("#responsive-main-menu").slideUp();
+    $(".sub-responsive-main-menu").slideUp();
+  };
+
+  $(window).resize(function () {
+    upSlide();
+  });
+
+  $(window).scroll(function (){
+    upSlide();
+  });
+
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 360) {
+      $('#back-top').fadeIn();
+    }else {
+      $('#back-top').fadeOut();
+    }
+  });
+
+  $("#back-top").click(function(){
+    $("html, body").animate({
+      scrollTop: 0
+    },800)
+  });
+
   $(".owl-carousel").owlCarousel({
     loop: true,
     margin: 10,
@@ -9,5 +45,4 @@ $(document).ready(function () {
       },
     },
   });
-
 });
