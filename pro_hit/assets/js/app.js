5c2b67ee0ced61e96225a6154d28114ac4b44927
Nguyễn Xuân Trường <nxt160602@gmail.com>
2021-12-26 09:19:36 +0700

    FIX BTL HIT

--- pro_hit/assets/js/app.js
+++ pro_hit/assets/js/app.js
@@ -17,28 +17,50 @@ $(document).ready(function () {
     upSlide();
   });
 
-  $(window).scroll(function (){
+  $(window).scroll(function () {
     upSlide();
   });
 
-  $(window).scroll(function(){
-    if($(this).scrollTop() > 360) {
-      $('#back-top').fadeIn();
-    }else {
-      $('#back-top').fadeOut();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 360) {
+      $("#back-top").fadeIn();
+    } else {
+      $("#back-top").fadeOut();
     }
   });
 
-  $("#back-top").click(function(){
-    $("html, body").animate({
-      scrollTop: 0
-    },800)
+  $("#back-top").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      800
+    );
   });
 
-  $(".owl-carousel").owlCarousel({
+  $(".owl-carousel-1").owlCarousel({
     loop: true,
     margin: 10,
     nav: true,
+    autoPlay: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      767: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+
+  $(".owl-carousel-2").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoPlay: true,
     responsive: {
       0: {
         items: 1,
